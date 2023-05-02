a3997f4b2e76eec64caf1cd678c723fafa32eca9
jiawei-zhang-a <jz4721@nyu.edu>
2023-05-01 21:33:31 -0400

    Exclude Y in all M

--- Post-Prediction-Imputation/Power.py
+++ Post-Prediction-Imputation/Power.py
@@ -19,7 +19,7 @@ beta_coef = None
 task_id = 1
 save_file = False
 max_iter = 10
-L = 1000
+L = 5000
 
 def run(Nsize, Unobserved, Single, filepath):
 
@@ -120,13 +120,13 @@ if __name__ == '__main__':
     for coef in np.arange(0.02,0.2,0.02):
         beta_coef = coef
         run(1000, Unobserved = 0, Single = 1 , filepath = "Result/HPC_power_1000" + "_single")
-        run(1000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_1000" + "_single")
-        run(2000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_2000" + "_single")
+        #run(1000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_1000" + "_single")
+        #run(2000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_2000" + "_single")
         run(2000, Unobserved = 0, Single = 1 , filepath = "Result/HPC_power_2000" + "_single")
-        """run(2000, Unobserved = 1, Single = False, filepath = "Result/HPC_power_unobserved_2000" + "_multi")
+        #run(2000, Unobserved = 1, Single = False, filepath = "Result/HPC_power_unobserved_2000" + "_multi")
         run(2000, Unobserved = 0, Single = False , filepath = "Result/HPC_power_2000" + "_multi")
-        run(1000, Unobserved = 1, Single = False , filepath = "Result/HPC_power_unobserved_1000" + "_multi")
-        run(1000, Unobserved = 0, Single = False, filepath = "Result/HPC_power_1000" + "_multi")  """
+        #run(1000, Unobserved = 1, Single = False , filepath = "Result/HPC_power_unobserved_1000" + "_multi")
+        run(1000, Unobserved = 0, Single = False, filepath = "Result/HPC_power_1000" + "_multi")  
       
 
         
