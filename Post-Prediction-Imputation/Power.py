77fcf02c1dd2b753edce6a777261042f24c0a1c4
jiawei-zhang-a <jz4721@nyu.edu>
2023-05-05 22:45:37 -0400

    MAINT

--- Post-Prediction-Imputation/Power.py
+++ Post-Prediction-Imputation/Power.py
@@ -116,16 +116,16 @@ if __name__ == '__main__':
     if os.path.exists("Result") == False:
         os.mkdir("Result")
 
-    for coef in np.arange(0.01,0.2,0.01):
+    for coef in np.arange(0.01,0.3,0.01):
         beta_coef = coef
         run(1000, Unobserved = 0, Single = 1 , filepath = "Result/HPC_power_1000" + "_single")
         run(1000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_1000" + "_single")
         run(2000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_2000" + "_single")
         run(2000, Unobserved = 0, Single = 1 , filepath = "Result/HPC_power_2000" + "_single")
-        run(2000, Unobserved = 1, Single = False, filepath = "Result/HPC_power_unobserved_2000" + "_multi")
-        run(2000, Unobserved = 0, Single = False , filepath = "Result/HPC_power_2000" + "_multi")
-        run(1000, Unobserved = 1, Single = False , filepath = "Result/HPC_power_unobserved_1000" + "_multi")
-        run(1000, Unobserved = 0, Single = False, filepath = "Result/HPC_power_1000" + "_multi")  
+        #run(2000, Unobserved = 1, Single = False, filepath = "Result/HPC_power_unobserved_2000" + "_multi")
+        #run(2000, Unobserved = 0, Single = False , filepath = "Result/HPC_power_2000" + "_multi")
+        #run(1000, Unobserved = 1, Single = False , filepath = "Result/HPC_power_unobserved_1000" + "_multi")
+        #run(1000, Unobserved = 0, Single = False, filepath = "Result/HPC_power_1000" + "_multi")  
 
         
 
