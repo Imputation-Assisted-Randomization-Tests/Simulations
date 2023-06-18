1c4596a1869aaae54fe7e70fb8a4c3c69a9b552f
jiawei-zhang-a <jz4721@nyu.edu>
2023-06-19 00:21:02 +0800

    BUG fix: sum8

--- Post-Prediction-Imputation/Simulation.py
+++ Post-Prediction-Imputation/Simulation.py
@@ -124,8 +124,8 @@ class DataGenerator:
     for p in range(1,6):
       for p_2 in range(1,6):
         for p_3 in range(1,6):
-          sum7 += X[:,p-1] * X[:,p_2-1] * np.sin(X[:,p_3-1])
-    sum8 = (1.0  / np.sqrt(5 * 5 * 5)) * sum7    
+          sum8 += X[:,p-1] * X[:,p_2-1] * np.sin(X[:,p_3-1])
+    sum8 = (1.0  / np.sqrt(5 * 5 * 5)) * sum8   
 
     U = U.reshape(-1,)
     Z = Z.reshape(-1,)
