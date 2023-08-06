92d765df6f3b37c5fda9fe0e12c37555983b0652
jiawei-zhang-a <jz4721@nyu.edu>
2023-08-05 22:21:18 -0400

    Big Sur: plot update

--- Post-Prediction-Imputation/plot_power_comparison.py
+++ Post-Prediction-Imputation/plot_power_comparison.py
@@ -3,78 +3,62 @@ import pandas as pd
 from analysis_power import read_npz_files
 import matplotlib.pyplot as plt
 
-def plot_results1(data, title):
-    columns = ['beta', 'MICE_Median', 'MICE_Linear',  'MICE_LightGBM', 'MICE_Oracle', 'MICE_xgboost']
-
-    df = pd.DataFrame(data, columns=columns)
-
-    plt.figure(figsize=(10, 6))
-
-    colors = {'Median': 'blue', 'Linear': 'red', 'LightGBM': 'orange', 'Oracle':'purple',   'xgboost':'green'}
-    linestyles = {'MICE': '-'}
-
-    for col in columns[1:]:
-        method = col.split('_')[1]
-        dataset = col.split('_')[0]
-        linestyle = linestyles[dataset]
-        plt.plot(df['beta'], df[col], marker='o', label=col, color=colors[method], linestyle=linestyle)
-        
-    plt.xlabel('Beta')
-    plt.ylabel('Power')
-    plt.title(title)
-    plt.legend()
-    plt.grid()
-    plt.show()
 
 def plot_results(data, title):
-    columns = ['beta', 'Imputer_Median', 'Imputer_Linear',  'Imputer_LightGBM', 'Imputer_Oracle', 'Imputer_xgboost']
+    columns = ['beta', 'Imputer_Median', 'Imputer_LinearRegression',  'Imputer_GradientBoosting', 'Imputer_Oracle']
 
     df = pd.DataFrame(data, columns=columns)
 
     plt.figure(figsize=(10, 6))
 
-    colors = {'Median': 'blue', 'Linear': 'red', 'LightGBM': 'orange',  'Oracle':'purple', 'xgboost':'green'}
+    colors = {'Median': 'blue', 'LinearRegression': 'red', 'GradientBoosting': 'orange', 'Oracle':'purple'}
     linestyles = {'Imputer': '-'}
 
     for col in columns[1:]:
         method = col.split('_')[1]
         dataset = col.split('_')[0]
         linestyle = linestyles[dataset]
-        plt.plot(df['beta'], df[col], marker='o', label=col, color=colors[method], linestyle=linestyle)
+        plt.plot(df['beta'], df[col], marker='o', label=method, color=colors[method], linestyle=linestyle)
         
     plt.xlabel('Beta')
     plt.ylabel('Power')
     plt.title(title)
     plt.legend()
     plt.grid()
-    plt.show()
+    #plt.show()
+    if not os.path.exists("pic"):
+        os.makedirs("pic")
 
-def plot(range,dir,title):
+    plt.savefig("pic/"+title+".png", format='png', dpi=600) 
+
+def plot(range,dir,title, small_size):
     print(range)
     data = []
     for coef in range:
         row_power = [coef]
         print("Result/%s/%f" % (dir,coef))
         for directory in ["Result/%s/%f" % (dir,coef)]:
-            results = read_npz_files(directory)
-            row_power.extend([results['median_power'], results['lr_power'], results['lightGBM_power'],results['oracle_power'],results['xgboost_power']])
+            results = read_npz_files(directory,small_size=small_size)
+            if small_size:
+                row_power.extend([results['median_power'], results['lr_power'], results['xgboost_power'],results['oracle_power']])
+            else:
+                row_power.extend([results['median_power'], results['lr_power'],results['lightGBM_power'], results['oracle_power']])
         data.append(row_power)
-    print(data)
     plot_results(data,title) 
 
 
 def main():
 
-    plot(np.arange(0,1.5,0.3),"HPC_power_100_unobserved_linearZ_linearX" + "_single","Size-100, linearZ_linearX, U")
+    plot(np.arange(0,1.5,0.25),"HPC_power_50_unobserved_linearZ_linearX" + "_single","Size-50, Single:linearZ_linearX", small_size=True)
 
-    plot(np.arange(0.0,0.4,0.08),"HPC_power_1000_unobserved_linearZ_linearX" + "_single","Size-1000, linearZ_linearX, U")
+    plot(np.arange(0.0,0.42,0.07),"HPC_power_1000_unobserved_linearZ_linearX" + "_single","Size-1000, Single: linearZ,linearX", small_size=False)
 
-    plot(np.arange(0.0,5,1),"HPC_power_100_unobserved_linearZ_nonlinearX" + "_single","Size-100, linearZ_nonlinearX, U")
+    plot(np.arange(0.0,4.8,0.8),"HPC_power_50_unobserved_linearZ_nonlinearX" + "_single","Size-50, Single: linearZ,nonlinearX", small_size=True)
 
-    plot(np.arange(0.0,0.80,0.16),"HPC_power_1000_unobserved_linearZ_nonlinearX" + "_single","Size-1000, linearZ_nonlinearX, U")
+    plot(np.arange(0.0,1.08,0.18),"HPC_power_1000_unobserved_linearZ_nonlinearX" + "_single","Size-1000, Single: linearZ,nonlinearX", small_size=False)
 
-    plot(np.arange(0.0,1.5,0.25),"HPC_power_100_unobserved_nonlinearZ_nonlinearX" + "_single","Size-100, nonlinearZ_nonlinearX, U")
+    plot(np.arange(0.0,1.5,0.25),"HPC_power_50_unobserved_nonlinearZ_nonlinearX" + "_single","Size-50, Single: nonlinearZ,nonlinearX", small_size=True)
 
-    plot(np.arange(0.0,0.3 ,0.05),"HPC_power_1000_unobserved_nonlinearZ_nonlinearX" + "_single","Size-1000, nonlinearZ_nonlinearX, U")
+    plot(np.arange(0.0,0.36,0.06),"HPC_power_1000_unobserved_nonlinearZ_nonlinearX" + "_single","Size-1000, Single: nonlinearZ,nonlinearX", small_size=False)
 
 main()
