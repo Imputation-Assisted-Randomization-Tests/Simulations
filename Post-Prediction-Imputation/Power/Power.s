ff7b82391e9507090b7197260269dca62b1daae0
jiawei-zhang-a <jz4721@nyu.edu>
2023-08-10 21:23:18 -0400

    MAINT

--- Post-Prediction-Imputation/Power/Power.s
+++ Post-Prediction-Imputation/Power/Power.s
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=Power
 #SBATCH --nodes=1
-#SBATCH --time=33:29:00
+#SBATCH --time=79:29:00
 #SBATCH --mem=1GB
 #SBATCH --cpus-per-task=1
 #SBATCH --output=Runtime/%a.out
@@ -12,7 +12,6 @@ module purge
 
 source /scratch/jz4721/Post-prediction-Causal-Inference/venv/bin/activate
 export PATH=/scratch/jz4721/Post-prediction-Causal-Inference/venv/lib64/python3.8/bin:$PATH
-source ~/.bashrc
 
 cd ../
 python Power.py $SLURM_ARRAY_TASK_ID
