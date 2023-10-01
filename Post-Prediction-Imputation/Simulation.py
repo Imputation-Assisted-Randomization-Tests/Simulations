993f9f67c3114fc482b9ea7ba2d25291c16978d5
jiawei-zhang-a <jz4721@nyu.edu>
2023-09-30 23:04:00 -0400

    MAINT

--- Post-Prediction-Imputation/Simulation.py
+++ Post-Prediction-Imputation/Simulation.py
@@ -4,8 +4,6 @@ import pandas as pd
 from scipy.stats import logistic
 
 
-
-
 class DataGenerator:
   def __init__(self,*, N = 1000, strata_size = 10, beta_11 = 1, beta_12 = 1, beta_21 = 1, beta_22 = 1, beta_23 = 1, beta_31 = 1,beta_32 = 1, MaskRate = 0.3, Unobserved = True, Single = True,  verbose = False, bias = False, Missing_lambda= None):
     self.N = N
@@ -159,10 +157,10 @@ class DataGenerator:
     Y_n1 = 1/4 * self.beta_11 * Z + self.beta_12 * Z * sum1   + sum2 + sum3 + np.sin(U) + StrataEps[:,0]  + IndividualEps[:,0]
 
     # Compute Yn2
-    Y_n2 =  self.beta_22 * Z * (X[:,0])**2 +self.beta_22 * Z * U**2 + sum4 + StrataEps[:,1] + IndividualEps[:,1]
+    Y_n2 = self.beta_11 * Z + self.beta_22 * Z * (X[:,0])**2 +self.beta_22 * Z * U**2 - sum4 + StrataEps[:,1] + IndividualEps[:,1]
 
     # Compute Yn3
-    Y_n3 = self.beta_32 * Z * sum5  + sum6 + sum8 + U +  StrataEps[:,2]  + IndividualEps[:,2]
+    Y_n3 = self.beta_11 * Z + self.beta_32 * Z * sum5  + sum6 + sum8 + U +  StrataEps[:,2]  + IndividualEps[:,2]
   
     Y = np.concatenate((Y_n1.reshape(-1, 1), Y_n2.reshape(-1, 1),Y_n3.reshape(-1, 1)), axis=1) 
   
