c3153c6b6119cd0dd683436804ed3b26a71f0ffe
jiawei-zhang-a <jz4721@nyu.edu>
2023-10-01 22:43:35 -0400

    MAINT

--- Post-Prediction-Imputation/Simulation.py
+++ Post-Prediction-Imputation/Simulation.py
@@ -159,10 +159,10 @@ class DataGenerator:
     Y_n1 = self.beta_11 * Z + self.beta_12 * Z * sum1   + sum2 +sum4 + np.sin(U) + StrataEps[:,0]  + IndividualEps[:,0]
 
     # Compute Yn2
-    Y_n2 = self.beta_22 * Z * (X[:,0])**2 +self.beta_22 * Z *(U)**2 + sum3 + sum8 + StrataEps[:,1] + IndividualEps[:,1]
+    Y_n2 = self.beta_11 * Z + self.beta_22 * Z * (X[:,0])**2 + self.beta_22 * Z *(U)**2 + sum3 + sum8 + StrataEps[:,1] + IndividualEps[:,1]
 
     # Compute Yn3
-    Y_n3 = self.beta_11 * Z + self.beta_32 * Z * sum5 + sum10 + U +  StrataEps[:,2]  + IndividualEps[:,2]
+    Y_n3 = self.beta_11 * Z + self.beta_32 * Z * sum5 + sum3 + U +  StrataEps[:,2]  + IndividualEps[:,2]
   
     Y = np.concatenate((Y_n1.reshape(-1, 1), Y_n2.reshape(-1, 1),Y_n3.reshape(-1, 1)), axis=1) 
   
